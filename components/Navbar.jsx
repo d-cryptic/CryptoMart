987013013f832aad18bbac9cbe9d13f81409fd59
Barun Debnath <barundebnath91@gmail.com>
2022-06-12 19:35:07 +0530

    navbar started | dark-light theme toggle done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,9 +9,34 @@ import images from '../assets';
 const Navbar = () => {
   const { theme, setTheme } = useTheme();
 
+  return (
+    <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1">
+      <div className="flex flex-1 flex-row justify-start">
+        <Link href="/">
+          <div
+            className="flexCenter md:hidden cursor-pointer"
+            onClick={() => {}}
+          >
+            <Image src={images.logo02} objectFit="contain" width={32} height={32} alt="Logo" />
+            <p className="dark:text-white text-nft-black-1 font-semibold  text-lg ml-1">CryptoMart</p>
+          </div>
+        </Link>
+        <Link href="/">
+          <div className="hidden md:flex" onClick={() => { }}>            <Image src={images.logo02} objectFit="contain" width={32} height={32} alt="Logo" /></div>
+        </Link>
+      </div>
 
-	return (
-	  
+      <div className="flex flex-initial flex-row justify-end">
+        <div className="flex items-center mr-2">
+          <input type="checkbox" className="checkbox" id="checkbox" onChange={() => setTheme(theme === 'light' ? 'dark' : 'light')} />
+          <label htmlFor="checkbox" className="flexBetween w-8  h-4 bg-black rounded-2xl p-1 relative label">
+            <i className="fas fa-sun" />
+            <i className="fas fa-moon" />
+            <div className="w-3 h-3 absolute bg-white rounded-full ball" />
+          </label>
+        </div>
+      </div>
+    </nav>
   );
 };
 export default Navbar;
