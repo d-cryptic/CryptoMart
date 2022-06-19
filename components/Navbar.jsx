c4b0895c3deecee77063349a4d830e4125063434
Barun Debnath <barundebnath91@gmail.com>
2022-06-19 15:58:43 +0530

    creator card done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,30 +1,42 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { useRouter } from 'next/router';
-import { useTheme } from 'next-themes';
-import Image from 'next/image';
-import Link from 'next/link';
+import React, { useState, useEffect, useContext } from "react";
+import { useRouter } from "next/router";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import Link from "next/link";
 
-import images from '../assets';
-import { Button } from '.';
+import images from "../assets";
+import { Button } from ".";
 
 const MenuItems = ({ isMobile, active, setActive }) => {
   const generateLink = (i) => {
-	  switch (i) {
-      case 0: return '/';
-      case 1: return '/created-nfts';
-      case 2: return '/my-nfts';
-      default: return '/';
-	  }
+    switch (i) {
+      case 0:
+        return "/";
+      case 1:
+        return "/created-nfts";
+      case 2:
+        return "/my-nfts";
+      default:
+        return "/";
+    }
   };
 
   return (
-    <ul className={`list-none flexCenter flex-row ${isMobile && 'flex-col h-full'}`}>
-      {['Explore NFTs', 'Listed NFTs', 'My NFTs'].map((item, i) => (
+    <ul
+      className={`list-none flexCenter flex-row ${
+        isMobile && "flex-col h-full"
+      }`}>
+      {["Explore NFTs", "Listed NFTs", "My NFTs"].map((item, i) => (
         <li
           key={i}
-          onClick={() => { setActive(item); }}
-          className={`flex flex-row items-center font-poppins font-semibold text-base dark:hover:text-white hover:text-nft-dark mx-3  ${active === item ? 'dark:text-white text-nft-black-1' : 'dark:text-nft-gray-3 text-nft-gray-2'}`}
-        >
+          onClick={() => {
+            setActive(item);
+          }}
+          className={`flex flex-row items-center font-poppins font-semibold text-base dark:hover:text-white hover:text-nft-dark mx-3  ${
+            active === item
+              ? "dark:text-white text-nft-black-1"
+              : "dark:text-nft-gray-3 text-nft-gray-2"
+          }`}>
           <Link href={generateLink(i)}>{item}</Link>
         </li>
       ))}
@@ -39,16 +51,22 @@ const ButtonGroup = ({ setActive, router }) => {
       btnName="Create"
       classStyles="mx-2 rounded-xl"
       handleClick={() => {
-		  setActive('');
-		  router.push('/create-nft');
-	  }}
+        setActive("");
+        router.push("/create-nft");
+      }}
     />
-  ) : <Button btnName="Connect" classStyles="mx-2 rounded-xl" handleClick={() => {}} />;
+  ) : (
+    <Button
+      btnName="Connect"
+      classStyles="mx-2 rounded-xl"
+      handleClick={() => {}}
+    />
+  );
 };
 
 const Navbar = () => {
   const { theme, setTheme } = useTheme();
-  const [active, setActive] = useState('Explore NFTs');
+  const [active, setActive] = useState("Explore NFTs");
   const [isOpen, setIsOpen] = useState(false);
   const router = useRouter();
 
@@ -58,21 +76,43 @@ const Navbar = () => {
         <Link href="/">
           <div
             className="flexCenter md:hidden cursor-pointer"
-            onClick={() => {}}
-          >
-            <Image src={images.logo02} objectFit="contain" width={32} height={32} alt="Logo" />
-            <p className="dark:text-white text-nft-black-1 font-semibold  text-lg ml-1">CryptoMart</p>
+            onClick={() => {}}>
+            <Image
+              src={images.logo02}
+              objectFit="contain"
+              width={32}
+              height={32}
+              alt="Logo"
+            />
+            <p className="dark:text-white text-nft-black-1 font-semibold  text-lg ml-1">
+              CryptoMart
+            </p>
           </div>
         </Link>
         <Link href="/">
-          <div className="hidden md:flex" onClick={() => { }}>            <Image src={images.logo02} objectFit="contain" width={32} height={32} alt="Logo" /></div>
+          <div className="hidden md:flex" onClick={() => {}}>
+            <Image
+              src={images.logo02}
+              objectFit="contain"
+              width={32}
+              height={32}
+              alt="Logo"
+            />
+          </div>
         </Link>
       </div>
 
       <div className="flex flex-initial flex-row justify-end">
         <div className="flex items-center mr-2">
-          <input type="checkbox" className="checkbox" id="checkbox" onChange={() => setTheme(theme === 'light' ? 'dark' : 'light')} />
-          <label htmlFor="checkbox" className="flexBetween w-8  h-4 bg-black rounded-2xl p-1 relative label">
+          <input
+            type="checkbox"
+            className="checkbox"
+            id="checkbox"
+            onChange={() => setTheme(theme === "light" ? "dark" : "light")}
+          />
+          <label
+            htmlFor="checkbox"
+            className="flexBetween w-8  h-4 bg-black rounded-2xl p-1 relative label">
             <i className="fas fa-sun" />
             <i className="fas fa-moon" />
             <div className="w-3 h-3 absolute bg-white rounded-full ball" />
@@ -87,7 +127,7 @@ const Navbar = () => {
       </div>
 
       <div className="hidden md:flex ml-2">
-        { isOpen ? (
+        {isOpen ? (
           <Image
             src={images.cross}
             objectFit="contain"
@@ -95,7 +135,7 @@ const Navbar = () => {
             height={20}
             alt="close"
             onClick={() => setIsOpen(false)}
-            className={theme === 'light' && 'filter invert'}
+            className={theme === "light" && "filter invert"}
           />
         ) : (
           <Image
@@ -105,22 +145,21 @@ const Navbar = () => {
             height={25}
             alt="menu"
             onClick={() => setIsOpen(true)}
-            className={theme === 'light' && 'filter invert'}
+            className={theme === "light" && "filter invert"}
           />
-			  ) }
+        )}
 
-        { isOpen && (
-        <div className="fixed inset-9 top-65 dark:bg-nft-dark bg-white z-30 nav-h flex justify-between flex-col">
-          <div className="flex-1 p-4">
-            <MenuItems active={active} setActive={setActive} isMobile />
+        {isOpen && (
+          <div className="fixed inset-9 top-65 dark:bg-nft-dark bg-white z-30 nav-h flex justify-between flex-col">
+            <div className="flex-1 p-4">
+              <MenuItems active={active} setActive={setActive} isMobile />
+            </div>
+            <div className="p-4 border-t dark:border-nft black-1 border-nft-gray-1">
+              <ButtonGroup setActive={setActive} router={router} />
+            </div>
           </div>
-          <div className="p-4 border-t dark:border-nft black-1 border-nft-gray-1">
-            <ButtonGroup setActive={setActive} router={router} />
-          </div>
-        </div>
-			  )}
+        )}
       </div>
-
     </nav>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,43 @@
-const Home = () => (
-  <h1 className="text-3xl font-bold underline">Hello world!</h1>
-);
+import React, { useState, useEffect, useRef } from "react";
+import { Banner, CreatorCard } from "../components";
+import images from "../assets";
+import { makeId } from "../utils/makeId";
+
+const Home = () => {
+  const parentRef = useRef(null);
+  const scrollRef = useRef(null);
+  console.log(makeId(3));
+  return (
+    <div className="flex justify-center sm:px-4 p-12">
+      <div className="w-full minmd:w-4/5">
+        <Banner
+          name="Discover, collect and sell extraordinary NFTs"
+          childStyle="md:text-4xl sm:text-2xl xs:text-xl text-left"
+          parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
+        />
+        <div>
+          <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl  font-semibold ml-4 xs:ml-0">
+            Best Creators
+          </h1>
+          <div className="relative flex-1 max-w-full flex mt-3" ref={parentRef}>
+            <div
+              className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none"
+              ref={scrollRef}>
+              {[6, 7, 8, 9, 10].map((i) => (
+                <CreatorCard
+                  key={`creator-${i}`}
+                  rank={i}
+                  creatorImg={images[`creator${i}`]}
+                  creatorName={`0x${makeId(3)}...${makeId(4)}`}
+                  creatorEths={10 - i * 0.5}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
