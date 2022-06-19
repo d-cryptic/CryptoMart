9f30b88a0c6c323cec33484f79b7ba40df9246da
Barun Debnath <barundebnath91@gmail.com>
2022-06-19 16:13:16 +0530

    scroll bar creator slider done

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,29 @@
 import React, { useState, useEffect, useRef } from "react";
+import Image from "next/image";
+
+import { useTheme } from "next-themes";
+
 import { Banner, CreatorCard } from "../components";
 import images from "../assets";
 import { makeId } from "../utils/makeId";
 
 const Home = () => {
+  const { theme } = useTheme();
   const parentRef = useRef(null);
   const scrollRef = useRef(null);
-  console.log(makeId(3));
+
+  const handleScroll = (direction) => {
+    const { current } = scrollRef;
+
+    const scrollAmount = window.innerWidth > 1800 ? 270 : 210;
+
+    if (direction === "left") {
+      current.scrollLeft -= scrollAmount;
+    } else {
+      current.scrollLeft += scrollAmount;
+    }
+  };
+
   return (
     <div className="flex justify-center sm:px-4 p-12">
       <div className="w-full minmd:w-4/5">
@@ -32,6 +49,29 @@ const Home = () => {
                   creatorEths={10 - i * 0.5}
                 />
               ))}
+
+              <div
+                onClick={() => handleScroll("left")}
+                className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer left-0">
+                <Image
+                  src={images.left}
+                  layout="fill"
+                  objectFit="contain"
+                  alt="left_arrow"
+                  className={theme === "light" && "filter invert"}
+                />
+              </div>
+              <div
+                onClick={() => handleScroll("right")}
+                className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer right-0">
+                <Image
+                  src={images.right}
+                  layout="fill"
+                  objectFit="contain"
+                  alt="left_arrow"
+                  className={theme === "light" && "filter invert"}
+                />
+              </div>
             </div>
           </div>
         </div>
