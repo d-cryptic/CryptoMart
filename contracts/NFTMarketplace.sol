71aa96af8dd9d85389232d7c2efbdd576332c593
Barun Debnath <barundebnath91@gmail.com>
2022-06-19 22:45:49 +0530

    all solidity functions done

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -128,4 +128,84 @@ abstract contract NFTMarketplace is ERC721URIStorage {
 
 	}
 
+	// fetch market items
+	function fetchMarketItems() public view returns (MarketItem[] memory){
+
+		// current item count
+		uint itemCount = _tokenIds.current();
+
+		// current unsold item count
+		uint unsoldItemCount = _tokenIds.current() - _itemsSold.current();
+
+		uint currentIndex = 0;
+
+		MarketItem[] memory items = new MarketItem[](unsoldItemCount);
+
+		for(uint i=0; i<itemCount; i++){
+			if(idToMarketItem[i+1].owner == address(this)){
+				uint currentId = i+1;
+				MarketItem storage currentItem = idToMarketItem[currentId];
+				items[currentIndex] = currentItem;
+				currentIndex += 1;
+			}
+		}
+
+		return items;
+	}
+
+	// Fetch my NFTs
+	function fetchMyNFTs() public view returns (MarketItem[] memory){
+		uint totalItemCount = _tokenIds.current();
+		uint itemCount = 0;
+		uint currentIndex = 0;
+
+		// get total items that a particular person has
+		for(uint i=0; i<totalItemCount; i++){
+			if(idToMarketItem[i+1].owner == msg.sender){
+				itemCount += 1;
+			}
+		}
+
+		MarketItem[] memory items = new MarketItem[](itemCount);
+
+		for(uint i=0; i<totalItemCount; i++){
+			if(idToMarketItem[i+1].owner == msg.sender){
+				uint currentId = i+1;
+
+				MarketItem storage currentItem = idToMarketItem[currentId];
+				items[currentIndex] = currentItem;
+				currentIndex += 1;
+			}
+		}
+		return items;
+	}
+
+
+	// get lists of nfts that a specific person listed
+	function fetchItemsListed() public view returns (MarketItem[] memory){
+		uint totalItemCount = _tokenIds.current();
+		uint itemCount = 0;
+		uint currentIndex = 0;
+
+		for(uint i=0; i<totalItemCount; i++){
+			if(idToMarketItem[i+1].seller == msg.sender){
+				itemCount += 1;
+			}
+		}
+
+		MarketItem[] memory items = new MarketItem[](itemCount);
+
+		for(uint i=0; i<totalItemCount; i++){
+			if(idToMarketItem[i+1].seller == msg.sender){
+				uint currentId = i+1;
+				MarketItem storage currentItem = idToMarketItem[currentId];
+
+				items[currentIndex] = currentItem;
+				currentIndex += 1;
+			}
+		}
+
+		return items;
+	}
+
 }
